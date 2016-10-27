d963f74c2067740fd9234e1bcad9c86f66980315
shiqi shao <shaoshiqi1992@hotmail.com>
2016-10-27 20:18:08 +0800

    serveradd

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var http = require("http");
-http.createServer(function(request, response) {
-
-}).listen(process.env.PORT || 3000);
+var express = require('express'), //引入express模块
+    app = express(),
+    server = require('http').createServer(app);
+app.use('/',express.static(__dirname+'/'));
+server.listen(process.env.PORT || 3000);
